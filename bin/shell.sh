94014755d6e27a5aa923634633cb22c414abcbe9
talog <tee.talog@gmail.com>
2018-03-08 23:13:16 +0900

    additionally write to script: change shell

--- bin/shell.sh
+++ bin/shell.sh
@@ -9,5 +9,15 @@ function f_install_zsh() {
 
     # install zsh
     sudo yum -y install zsh
+
+    # change shell
+    type chsh >/dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        chsh -s /bin/zsh
+    else
+        echo "Please change shell"
+    fi
+
+    return 0
 }
 
