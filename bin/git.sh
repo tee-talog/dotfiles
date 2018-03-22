89d7dda6e757ced3f9c7ffcfa860b6c69f829e52
talog <tee.talog@gmail.com>
2018-03-22 23:11:08 +0900

    add git install script

---
+++ bin/git.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function f_install_git() {
+    # is Git installed?
+    type zsh >/dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        return 0
+    fi
+
+    # make temporary directory
+    local readonly _temp_dir_name="$(mktemp -d)"
+
+    # install Git
+    sudo yum install -y git-all
+}
+
