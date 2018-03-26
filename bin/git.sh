d6894920a7e99fb66769486270c38d59625fcd4a
talog <tee.talog@gmail.com>
2018-03-26 22:30:04 +0900

    delete unnecessary prosessing

--- bin/git.sh
+++ bin/git.sh
@@ -7,9 +7,6 @@ function f_install_git() {
     return 0
   fi
 
-  # make temporary directory
-  local readonly _temp_dir_name="$(mktemp -d)"
-
   # install Git
   type yum >/dev/null 2>&1
   if [ $? -eq 0 ]; then
