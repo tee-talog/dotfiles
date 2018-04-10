5a07315f7300c4dafe913f54e77df6f6ad91b080
talog <tee.talog@gmail.com>
2018-04-10 15:26:00 +0900

    refactor: redirect

--- bin/shell.sh
+++ bin/shell.sh
@@ -2,13 +2,13 @@
 
 function f_install_zsh() {
   # is zsh installed?
-  type zsh >/dev/null 2>&1
+  type zsh &>/dev/null
   if [ $? -eq 0 ]; then
     return 0
   fi
 
   # install zsh
-  type yum >/dev/null 2>&1
+  type yum &>/dev/null
   if [ $? -eq 0 ]; then
     sudo yum -y install zsh
   else
@@ -17,7 +17,7 @@ function f_install_zsh() {
   fi
 
   # change shell
-  type chsh >/dev/null 2>&1
+  type chsh &>/dev/null
   if [ $? -eq 0 ]; then
     chsh -s /bin/zsh
   else
